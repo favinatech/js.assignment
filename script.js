1541f6f34ac8f4b6e5c12c229921001e17d8408a
favinatech <favourahams1@gmail.com>
2025-01-27 19:28:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Example for ++i (Pre-increment)
+function preIncrementExample() {
+    let i = 1;
+    ++i;
+    let x = 10 * i;
+    console.log("Pre-increment example result (x):", x); // Output: 20
+}
+
+// Example for i++ (Post-increment)
+function postIncrementExample() {
+    let i = 1;
+    let z = 10 * i;
+    i++;
+    console.log("Post-increment example result (z):", z); // Output: 10
+}
